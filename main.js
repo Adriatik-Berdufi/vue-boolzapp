7d246cfd7b118e297164795775db0ca4020b9d2f
Adriatik Berdufi <adratik.berdufi@gmail.com>
2024-02-01 16:02:22 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,21 @@
+const {createApp} = Vue;
+const app = createApp({
+    data(){
+        return{
+            title: 'New Project',
+        };
+    },
+    methods: {
+       
+    },
+    
+    mounted(){
+        console.log('App montata');
+        
+    },
+});
+app.mount('#app');
+
+
+
+
